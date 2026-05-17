9a8c6831e75440e8fd78bc1014c90e2e8fd8118d
Peeeweee <kentpaulodelgado@gmail.com>
2026-05-17 12:48:39 +0800

    Rename Delgado_K to Delgado_Kent.jpg and update references

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,7 +6,7 @@ const developers = [
     name: "Kent Paulo Delgado",
     role: "Developer",
     // Use relative paths that Vite will process correctly or use the base URL
-    image: "developers/Delgado_K.jpg",
+    image: "developers/Delgado_Kent.jpg",
   },
   {
     name: "Earl Josh Delgado",
